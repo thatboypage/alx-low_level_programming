2afa4ef4208c05bc1b2c413c8a4ddcd4ea4204c2
thatboypage <richardolanite@gmail.com>
2022-09-09 15:35:09 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+		putchar(ch);
+	putchar(10);
+	return (0);
+}
