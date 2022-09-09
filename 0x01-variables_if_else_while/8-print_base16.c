8a668e9c24dd0509299e6b7e6d595f006ff0ff22
thatboypage <richardolanite@gmail.com>
2022-09-09 16:12:01 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+
+	putchar(10);
+
+	return (0);
+}
