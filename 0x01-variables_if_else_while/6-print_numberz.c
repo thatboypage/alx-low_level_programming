eab544c7ed8dea909a7b39ad13a3da8074c53e9a
thatboypage <richardolanite@gmail.com>
2022-09-09 16:00:03 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+		putchar(ch);
+	putchar(10);
+	return (0);
+}
